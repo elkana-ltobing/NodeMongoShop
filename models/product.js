8bec1ec48389bc262ab5040b722da2af98b47a8e
elkana-ltobing <elkana.ltobing@gmail.com>
2018-07-22 10:25:13 +0700

    update

--- models/product.js
+++ models/product.js
@@ -5,7 +5,9 @@ var schema = new Schema({
     imagePath: {type: String, required: true},
     title: {type: String, required: true},
     description: {type: String, required:true},
-    price: {type: Number, required: true}
+    price: {type: Number, required: true},
+    Likes: {type: Number, default:0}
+    
 });
 
 module.exports = mongoose.model('Product', schema);
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
+var formidable = require('formidable');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -18,19 +19,52 @@ router.get('/addNewProduct', function (req, res) {
     res.render('shop/addProduct', { title: 'Page Add' });
 });
 
-router.post('/addNewProduct', function (req, res) {
-    var product = new Product(req.body);
+// router.post('/addNewProduct', function (req, res) {
+//     var product = new Product(req.body);
 
-    product.save(function (err) {
-        if (err) {
-            console.log(err);
-            res.render("shop/addProduct");
-        } else {
-            console.log("Successfully created an product.");
-            res.redirect("/");
-        }
+//     product.save(function (err) {
+//         if (err) {
+//             console.log(err);
+//             res.render("shop/addProduct");
+//         } else {
+//             console.log("Successfully created an product.");
+//             res.redirect("/");
+//         }
+//     });
+// });
+
+router.post("/addNewProduct", function (req, res) {
+    var form = new formidable.IncomingForm();
+    var fullFilename;
+    var product = new Product();
+
+    form.parse(req, function (err, fields) {
+        product = new Product(fields);
+        console.log('Product:' + product);
+        console.log('fullfilename:' + fullFilename);
+        product.imagePath = fullFilename;
+        product.save(function (err) {
+            if (err) {
+                console.log(err);
+                res.render("shop/addProduct");
+            } else {
+                console.log("Successfully created a Product");
+                res.redirect("/");
+            }
+        });
+    })
+    form.on('file', function (name, file, fields) {
+        product = new Product(fields);
+        console.log('Product:' + product);
+        console.log('Uploaded:' + file.name);
+        fullFilename = './photo_uploads/' + file.name;
     });
+
+    form.on("fileBegin", function (name, file) {
+        file.path = process.cwd() + '/public/photo_uploads/' + file.name;
+    })
 });
+
 
 router.get('/list', function (req, res) {
     Product.find(function (err, prods) {
@@ -61,24 +95,72 @@ router.get('/edit/:id', function (req, res) {
 })
 
 router.post('/updateProduct/:id', function (req, res) {
+    var form = new formidable.IncomingForm();
+    var id = req.params.id;
+    var fullfilename;
+
+    form.parse(req, function (err, fields) {
+        console.log(fields);
+        Product.findById(id, function (err, doc) {
+            if (err) {
+                console.log("Find by id: " + err);
+                res.redirect('/edit/' + id);
+            }
+            console.log("Full filename: " + fullfilename);
+            if (!fullfilename) {
+                doc.imagePath = fields.imagePath;
+            }
+            else {
+                doc.imagePath = fullfilename;
+            }
+            doc.title = fields.title;
+            doc.description = fields.description;
+            doc.price = fields.price;
+            doc.save(function (err) {
+                if (err) {
+                    console.log("error sace: " + err);
+                    res.redirect('/edit/' + id);
+                } else {
+                    console.log("successfully saved edited product");
+                    res.redirect('/list');
+                }
+            })
+        })
+    })
+
+    form.on('file', function (name, file, fields) {
+        console.log('Filename ' + file.name);
+        if (file.name) {
+            fullfilename = './photo_uploads/' + file.name;
+            if(file.name){
+                fullfilename = './photo_uploads/'+ file.name;
+            }
+        }
+    });
+
+        form.on('fileBegin', function (name, file) {
+            console.log("Masuk upload, filename : "+file.name);
+            if(file.name){
+            file.path = process.cwd() + '/public/photo_uploads/' + file.name
+            }
+        });
+});
+
+router.get('/likes/:id', function (req, res) {
     Product.findByIdAndUpdate(req.params.id,
         {
-            $set:
+            $inc:
             {
-                imagePath: req.body.imagePath,
-                title: req.body.title,
-                description: req.body.description,
-                price: req.body.price
+                Likes: 1
             }
-        }, { new: true },
+        },
         function (err, products) {
             if (err) {
                 console.log(err);
-                res.render("shop/edit", { products: req.body });
+                res.render("/");
             }
-            res.redirect("/list");
+            res.redirect("/");
         });
 });
 
 module.exports = router;
-
